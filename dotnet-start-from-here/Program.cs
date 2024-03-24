c34b1152e34a2e4dc4d828bca8d8d47cf1e8ccd6
Binotto <binotto10@gmail.com>
2024-03-24 12:32:34 -0300

    Project created

---
+++ dotnet-start-from-here/Program.cs
@@ -0,0 +1,21 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
